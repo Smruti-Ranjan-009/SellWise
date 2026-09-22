181e1b1e51d23ac760737389e84a936a3c36d58c
Smruti-Ranjan-009 <smrutiranjankodamasingh19@gmail.com>
2026-09-22 14:08:15 +0530

    data_ingestion completed

---
+++ main.py
@@ -0,0 +1,15 @@
+from SellWise import logger
+from SellWise.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+# logger.info("Welcome to our SellWise package")
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
